e1047934c6059c5ee9a81c34c3286c280bf50bc2
thePanda-X <alex.asensi90@gmail.com>
2026-03-16 19:56:44 +0100

    fix: Update publisher name in package.json and exclude test-scripts in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ const extensionConfig = {
     rules: [
       {
         test: /\.ts$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /test-scripts/],
         use: [
           {
             loader: 'ts-loader'
